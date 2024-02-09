7cd07a73c0e8e33deeede94fc23e41ca9a3d9072
owen <wavefunctionc@gmail.com>
2024-02-10 01:27:09 +0900

    Add Race Condition & Data Race Class

--- Thread/src/main/java/com/thread/share/SharedClass.java
+++ Thread/src/main/java/com/thread/share/SharedClass.java
@@ -4,18 +4,39 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SharedClass {
-    int x = 0;
-    int y = 0;
+    public static void main(String[] args) {
+        SubSharedClass sharedClass = new SubSharedClass();
 
-    public void increment() {
-        x++;
-        y++;
+        Thread t1 = new Thread(() -> {
+            for (int i=0; i<Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i=0; i<Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+        });
+
+        t1.start();
+        t2.start();
     }
 
-    /* 불변성 체크 함수 */
-    public void checkForDataRace() {
-        if (y > x) {
-            log.error("불가능한 상황 발생");
+    public static class SubSharedClass {
+        private volatile int x = 0;
+        private volatile int y = 0;
+
+        public void increment() {
+            x++;
+            y++;
+        }
+
+        /* 불변성 체크 함수 */
+        public void checkForDataRace() {
+            if (y > x) {
+                log.error("y > x Data Race is Detected");
+            }
         }
     }
 }
