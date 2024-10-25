01e60f18e2863210a107efb62a78f02aaccb19a4
owen <floralexium@gmail.com>
2024-10-25 15:47:37 +0900

    Lock-Free Algorithm 글, 코드 추가

--- Thread/src/main/java/com/thread/lockfree/ECommerceInventoryCounter.java
+++ Thread/src/main/java/com/thread/lockfree/ECommerceInventoryCounter.java
@@ -1,12 +1,50 @@
 package com.thread.lockfree;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 public class ECommerceInventoryCounter {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter counter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(counter);
+        DecrementingThread decrementingThread = new DecrementingThread(counter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        log.info("현재 보유한 아이템 수 : {}", counter.getItems());
+    }
+
+    // 증가 스레드
+    @AllArgsConstructor
+    public static class IncrementingThread extends Thread {
+        private InventoryCounter inventoryCounter;
+
+        @Override
+        public void run() {
+            for (int i=0; i<10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
 
     // 감소 스레드
+    @AllArgsConstructor
     public static class DecrementingThread extends Thread {
-        private
+        private InventoryCounter inventoryCounter;;
+
+        @Override
+        public void run() {
+            for (int i=0; i<10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
     }
 
     // 인벤토리 카운터
@@ -18,7 +56,11 @@ public class ECommerceInventoryCounter {
         }
 
         public void decrement() {
+            items.decrementAndGet();
+        }
 
+        public int getItems() {
+            return items.get();
         }
     }
 }
